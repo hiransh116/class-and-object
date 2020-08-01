46ff8e661651a3ae3b1b31efb799ee1d15a6aa3d
hiransh116 <68466400+hiransh116@users.noreply.github.com>
2020-08-01 17:51:03 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# class-and-object
+# bouncyBall
+Bouncy Ball created using matter.js physics engine
---
+++ box.js
@@ -0,0 +1,29 @@
+class Box{
+constructor(x,y,width,height){
+ var options={
+     restitution:0.5,
+     friction:0.4,
+     density:0.5,
+     
+ }
+ this.body=Bodies.rectangle(x,y,width,height,options);
+    this.width=width;
+    this.height=height;
+ World.add(world,this.body);
+}
+display(){
+    push();
+    translate(this.body.position.x,this.body.position.y);
+    angleMode(RADIANS);
+    rotate(this.body.angle);
+rectMode(CENTER);
+rect(0,0,this.width,this.height);
+   pop();                                    
+}
+
+}
+
+
+
+
+
